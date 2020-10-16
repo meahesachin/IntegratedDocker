d1748eb2ddbf5dc27b45bcdde6f51e1b71a0ae15
meahesachin <meahesachin@gmail.com>
2020-10-15 20:50:25 -0400

    Task: Adding config files

--- my_script.py
+++ my_script.py
@@ -2,6 +2,7 @@
 # https://github.com/mmulqueen/pyStrich
 from pystrich.datamatrix import DataMatrixEncoder
 
+
 encoder = DataMatrixEncoder('This is a DataMatrix.')
 encoder.save('./datamatrix_test.png')
 print(encoder.get_ascii())
